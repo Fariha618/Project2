85fa703c90dc2909bb82506b94bcd26608058700
fariha618 <reza.fariha@gmail.com>
2019-08-02 03:23:42 +0600

    Bootstrap_Layout_And_Database_Contexts_Added

---
+++ BusinessManagementSystem/BusinessManagementSystem.DatabaseContext/DatabaseContext/BusinessDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessManagementSystem.Models.Models;
+
+namespace BusinessManagementSystem.DatabaseContext.DatabaseContext
+{
+    public class BusinessDbContext:DbContext
+    {
+        
+    }
+}
